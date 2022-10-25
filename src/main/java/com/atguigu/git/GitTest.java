dc404ff1262fd2f1373ef4c2da2aee6c7c08b5eb
women17sui <1584499844@qq.com>
2022-10-25 15:32:19 +0800

    hotfix test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git2222");
+        System.out.println("hotfix test");
 
     }
 
