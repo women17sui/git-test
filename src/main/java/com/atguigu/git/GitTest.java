c3b4b788e6b23e8b8fdf37ca10287916a3f49afe
women17sui <1584499844@qq.com>
2022-10-25 14:14:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @PROJECT_NAME: git-test
+ * @DESCRIPTION:
+ * @USER: women17sui
+ * @DATE: 2022/10/25 14:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
