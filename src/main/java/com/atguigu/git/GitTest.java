8d860211ef690b82eb85d678de768bd6936731b0
women17sui <1584499844@qq.com>
2022-10-25 15:22:14 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git2222");
+
     }
+
 
 }
